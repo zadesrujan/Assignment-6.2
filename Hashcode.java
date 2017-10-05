5ecf2055984ca8b3a395923defffd1e50265b203
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-10-05 16:00:50 +0530

    Add files via upload

---
+++ Hashcode.java
@@ -0,0 +1,18 @@
+package hashcode;
+
+public class Hashcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Word w1=new Word();//intializing the value to the word
+		Word w2=new Word();
+		System.out.println("Hash code="+w1.hashCode());//printing the hash code value 
+		//of the given variables
+		System.out.println("Hash code="+w2.hashCode());
+		
+	}
+
+
+	}
+
+
---
+++ Word.java
@@ -0,0 +1,12 @@
+package hashcode;
+
+public class Word {
+	String name;//declaring the values to the data type
+	 int rollno;
+	 void talk()//we are taking action of Hashcode as method
+	 {
+		 System.out.println("Student name:"+name);//printing the values
+		 System.out.println("Student rollno:"+rollno);
+	 
+	 }
+}
